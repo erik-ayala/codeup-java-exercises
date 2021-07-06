8ca99b0b6cd7b70c0543a1b71e83c41afd5aabb1
Erik Ayala <ayala.erik61@gmail.com>
2021-07-06 09:42:06 -0500

    pushed warmup

---
+++ src/Warmup/Parent.java
@@ -0,0 +1,7 @@
+package Warmup;
+
+public class Parent {
+    public static void main(String[] args) {
+        System.out.println("I'm the parent.");
+    }
+}
---
+++ src/Warmup/Subclass.java
@@ -0,0 +1,8 @@
+package Warmup;
+
+public class Subclass extends Parent {
+    public static void main(String[] args) {
+        System.out.println("I'm the subclass.");
+
+    }
+}
